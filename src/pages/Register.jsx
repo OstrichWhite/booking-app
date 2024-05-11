57aa2289e09c776704cee58ed275905b980e5355
OstrichWhite <rahimcsestudies@gmail.com>
2024-05-11 21:52:31 +0530

    Introduced registerUser.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,54 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+const inputHandler =
+  (setter) =>
+  ({ target: { value } }) =>
+    setter(value);
 
-const Register = () => (
-  <div className="mt-4 grow flex items-center justify-around">
-    <div className="mb-32">
-      <h1 className="text-4xl text-center mb-4">Register</h1>
-      <form action="" className="max-w-md mx-auto">
-        <input type="text" name="" id="" placeholder="John Doe" />
-        <input type="email" name="" id="" placeholder="your@email.com" />
-        <input type="password" name="" id="" placeholder="password" />
-        <button className="primary">Register</button>
-        <div className="py-2 text-center text-gray-500">
-          {"Already have an account?"}{" "}
-          <Link to={"/login"} className="underline text-black">
-            Login
-          </Link>
-        </div>
-      </form>
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const registerUser = (event) => {
+    event.preventDefault();
+    axios.post("/register", { name, email, password });
+  };
+
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-32">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form action="" className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            value={name}
+            onChange={inputHandler(setName)}
+            placeholder="John Doe"
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={inputHandler(setEmail)}
+            placeholder="your@email.com"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={inputHandler(setPassword)}
+            placeholder="password"
+          />
+          <button className="primary">Register</button>
+          <div className="py-2 text-center text-gray-500">
+            {"Already have an account?"}{" "}
+            <Link to={"/login"} className="underline text-black">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Register;
