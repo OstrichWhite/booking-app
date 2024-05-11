66d1925562a01e2d807157fdddaf89775c549fba
OstrichWhite <rahimcsestudies@gmail.com>
2024-05-11 21:50:48 +0530

    Set axios baseURL.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Layout from "./components/Layout";
 import Register from "./pages/Register";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000";
 
 const App = () => (
   <Routes>
